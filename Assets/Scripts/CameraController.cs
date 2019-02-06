8b77741ec10a43f7b63ad91b152a75c6f6f0d397
tiba666 <frehr@hotmail.com>
2019-02-06 09:12:59 +0100

    movement script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public Transform target;
+	public Vector3 offset;
+	private float currentzoom = 10f;
+	public float pitch = 2f;
+	private void LateUpdate()
+	{
+		transform.position = target.position - offset * currentzoom;
+		transform.LookAt(target.position + Vector3.up * pitch);
+	}
+
+}
--- Assets/Scripts/MovementBall.cs
+++ Assets/Scripts/MovementBall.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MovementBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
 }
