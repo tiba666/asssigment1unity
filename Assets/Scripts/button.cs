f0451d53f860dcfeff844c321de8a9c8b7ede26b
tiba666 <frehr@hotmail.com>
2019-02-06 08:40:05 +0100

    sounds and scripts

--- Assets/Scripts/button.cs
+++ Assets/Scripts/button.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class button : MonoBehaviour
 {
     public string name;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Vector3 position;
+	public GameObject player;
     public void prnitString()
     {
         print(name);
     }
+	public void playSound()
+	{
+		GetComponent<AudioSource>().Play();
+	}
+	public void spawnPlayer()
+	{
+		if (player != null)
+		{
+			Instantiate(player, position, Quaternion.identity);
+		}
+	}
 }
